c54b36e9ef4f829009a74bcf2ae6f7fad3d3ade1
handler-bird <powerk11@hotmail.de>
2024-12-07 15:19:48 +0100

    add torch.compile and validation loop

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ import pandas as pd
 import re
 import os
 import io
+from copy import deepcopy
+
 
 
 def training(config: dict, base_dir: str, device: str):
@@ -53,6 +55,7 @@ def training(config: dict, base_dir: str, device: str):
         torch_dtype=torch.float16,
     ).to(device)
     text_encoder.requires_grad_(False)
+    text_encoder = torch.compile(text_encoder)
 
     tokenizer = CLIPTokenizer.from_pretrained(
         config['model_name'],
@@ -68,6 +71,7 @@ def training(config: dict, base_dir: str, device: str):
         torch_dtype=torch.float16,
     ).to(device)
     vae.requires_grad_(False)
+    vae = torch.compile(vae)
 
     # inject LoRA
     unet_lora_params, _ = inject_trainable_lora(
@@ -75,6 +79,9 @@ def training(config: dict, base_dir: str, device: str):
         r=config['r'],
         dropout_p=config['dropout'],
     )
+
+    # compile unet
+    #unet = torch.compile(unet, mode='reduce-overhead')
 
     # prepare data
     image_transforms = transforms.Compose(
@@ -112,10 +119,10 @@ def training(config: dict, base_dir: str, device: str):
             ),
         }
 
-    # prepare dataloader
-    dataset = (
+    # train data
+    train_dataset = (
         wds.WebDataset(
-            f'./data/{config["dataset"]}/{config["style"]}_dataset.tar',
+            f'./data/{config["dataset"]}/{config["style"]}_dataset.tar', # TODO change path to train or test
             shardshuffle=1024,
         )
         .decode('pil')
@@ -123,8 +130,23 @@ def training(config: dict, base_dir: str, device: str):
         .map(apply_transform)
         .to_tuple('image', 'input_ids', 'attention_mask')
     )
-    dataloader = DataLoader(
-        dataset, config['batch_size'], shuffle=False, collate_fn=collate_fn
+    train_dataloader = DataLoader(
+        train_dataset, config['batch_size'], shuffle=False, collate_fn=collate_fn
+    )
+
+    # test data
+    test_dataset = (
+        wds.WebDataset(
+            f'./data/{config["dataset"]}/{config["style"]}_dataset.tar', # TODO change path to train or test
+            shardshuffle=1024,
+        )
+        .decode('pil')
+        .shuffle(1024)
+        .map(apply_transform)
+        .to_tuple('image', 'input_ids', 'attention_mask')
+    )
+    test_dataloader = DataLoader(
+        test_dataset, config['batch_size'], shuffle=False, collate_fn=collate_fn
     )
 
     def read_total_samples_from_tar(tar_filename):
@@ -135,9 +157,9 @@ def training(config: dict, base_dir: str, device: str):
                     total_metadata = json.load(f)
 
                     return total_metadata.get('total_samples', 0)
-
-    total_samples = read_total_samples_from_tar(f'{config["style"]}_dataset.tar')
-    epochs = config['epochs']
+    # get train and test length
+    train_samples = read_total_samples_from_tar(f'./data/{config["dataset"]}/{config["style"]}_dataset.tar') # TODO change path for train and test
+    test_samples = read_total_samples_from_tar(f'./data/{config["dataset"]}/{config["style"]}_dataset.tar')
 
     # prepare training parameters
     optimizer = AdamW8bit(
@@ -146,9 +168,14 @@ def training(config: dict, base_dir: str, device: str):
         weight_decay=config['weight_decay'],
     )
 
+    # loss function
     loss_fn = MSELoss()
 
-    num_training_steps = total_samples * epochs
+    # epochs
+    epochs = config['epochs']
+
+    # learning rate scheduler
+    num_training_steps = train_samples * epochs
     warmup_steps = int(num_training_steps * 0.1)
 
     lr_scheduler = get_scheduler(
@@ -158,21 +185,29 @@ def training(config: dict, base_dir: str, device: str):
         num_training_steps=num_training_steps,
     )
 
+    # losses
     losses = []
 
+    # gradient accumulation
     accumulation_steps = 32 // config['batch_size']
+
+    # Model saving & patience
+    current_loss = 1000
+    best_model_state = None
+    best_epoch = 0
+    patience = config['patience']
 
     for epoch in range(epochs):
         unet.train()
         running_loss = 0.0
 
         for step, batch in enumerate(
-            tqdm(dataloader, desc=f'Epoch:[{epoch + 1}|{epochs}]')
+            tqdm(train_dataloader, desc=f'Epoch:[{epoch + 1}|{epochs}]')
         ):
             optimizer.zero_grad(
                 set_to_none=True
             ) if step % accumulation_steps == 0 else None
-
+            
             # load data to device
             image = batch['image'].to(device)
             input_ids = batch['input_ids'].to(device)
@@ -221,18 +256,90 @@ def training(config: dict, base_dir: str, device: str):
             loss.backward()
 
             # gradient accumulation
-            if (step + 1) % accumulation_steps == 0 or (step + 1) == total_samples:
+            if (step + 1) % accumulation_steps == 0 or (step + 1) == train_samples:
                 torch.nn.utils.clip_grad_norm_(unet.parameters(), 1.0)
                 optimizer.step()
                 lr_scheduler.step()
 
-        epoch_loss = running_loss / total_samples
+        epoch_loss = running_loss / train_samples
         losses.append(epoch_loss)
         print(epoch_loss)
 
+        # test loop
+        if epoch % 1 == 0:
+            unet.eval()
+            test_loss = 0.0
+
+            with torch.no_grad():
+                for _, batch in enumerate(tqdm(test_dataloader)):
+                    # load data to device
+                    image = batch['image'].to(device)
+                    input_ids = batch['input_ids'].to(device)
+                    attention_mask = batch['attention_mask'].to(device)
+
+                    # create noise latents
+                    latents = vae.encode(image.to(dtype=torch.float16)).latent_dist.sample()
+                    latents = latents * 0.18215
+
+                    timesteps = torch.randint(
+                        0,
+                        scheduler.config.num_train_timesteps,
+                        (latents.shape[0],),
+                        device=device,
+                    )
+                    timesteps = timesteps.long()
+
+                    noise = torch.randn_like(latents)
+                    noise_latents = scheduler.add_noise(latents, noise, timesteps)
+
+                    # encode input
+                    encoder_hidden_states = text_encoder(input_ids, return_dict=False)[0]
+
+                    # forward pass
+                    output = unet(
+                        sample=noise_latents,
+                        timestep=timesteps,
+                        encoder_hidden_states=encoder_hidden_states,
+                        encoder_attention_mask=attention_mask,
+                    ).sample
+
+                    # get target value
+                    if scheduler.config.prediction_type == 'epsilon':
+                        target = noise
+                    elif scheduler.config.prediction_type == 'v_prediction':
+                        target = scheduler.get_velocity(latents, noise, timesteps)
+                    else:
+                        raise ValueError(
+                            f'Unknown prediction type {scheduler.config.prediction_type}'
+                        )
+
+                    # loss calculation
+                    loss = loss_fn(output.float(), target.float())
+                    test_loss += loss.item()
+                
+            print(f"Epoch {epoch + 1}, Test Loss: {test_loss / test_samples:.4f}")
+
+        if current_loss > loss.item():
+            current_loss = loss.item()
+
+            # save best model state
+            unet.to('cpu')
+            best_model_state = deepcopy(unet)
+            unet.to(device)
+
+            best_epoch = epoch
+            patience = config['patience']
+
+        else:
+            patience -= 1
+
+        if patience == 0:
+            break
+
+    print(f'best epoch {best_epoch}')
     print('LoRA training finished')
     print(losses)
-    save_lora_weight(unet, f'{base_dir}/lora_weight.pt')
+    save_lora_weight(best_model_state, f'{base_dir}/lora_weight.pt')
 
 
 def inference(config: dict, base_dir: str, device: str):
@@ -244,6 +351,10 @@ def inference(config: dict, base_dir: str, device: str):
         torch_dtype=torch.float16,
     ).to(device)
     pipe.safety_checker = None
+
+    # torch compile
+    pipe.unet = torch.compile(pipe.unet)
+    pipe.vae = torch.compile(pipe.vae)
 
     # create image without LoRA
     image = pipe(
@@ -337,6 +448,7 @@ def data_preparation(config: dict, base_dir: str):
         total_metadata_info.size = len(total_metadata_bytes)
         tar.addfile(total_metadata_info, io.BytesIO(total_metadata_bytes))
 
+        print(f'total length: {total_samples}')
         print(f'Tar archive created: {tar_archive}')
 
 
