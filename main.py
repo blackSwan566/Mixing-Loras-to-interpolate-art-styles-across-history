187278530321b577e0990e2b51a91f0aa8610cdb
blackSwan566 <fionamariele@gmail.com>
2025-01-20 12:48:20 +0100

    isolated push

---
+++ main.py
@@ -0,0 +1,79 @@
+import argparse
+from src.utils.functions import load_config
+from src.scripts.lora_fine_tune import training_lora
+from src.scripts.lora_inference import inference_lora
+from src.scripts.create_webdatasets import precompute_data
+from src.scripts.merge_loras_v1 import merge_loras_v1
+from src.scripts.merge_loras_v2 import merge_loras_v2
+from src.scripts.merge_loras_v3 import merge_loras_v3
+from src.scripts.merge_loras_v4 import merge_loras_v4
+import torch
+
+
+def main(args):
+    config, base_dir = load_config(task=args.task)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    if args.task == 'training':
+        training_lora(config, base_dir, device)
+
+    elif args.task == 'inference':
+        inference_lora(config, base_dir, device)
+
+    elif args.task == 'precompute':
+        precompute_data(config, device)
+    
+    elif args.task == 'merge_loras_v1':
+        merge_loras_v1(config, base_dir, device)
+        
+    elif args.task == 'merge_loras_v2':
+        merge_loras_v2(config, base_dir, device)
+        
+    elif args.task == 'merge_loras_v3':
+        merge_loras_v3(config, base_dir, device)
+        
+    elif args.task == 'merge_loras_v4':
+        merge_loras_v3(config, base_dir, device)
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Fine Tune diffusion model')
+
+    subparsers = parser.add_subparsers(
+        dest='task', required=True, help='run specific task'
+    )
+
+    training_parser = subparsers.add_parser(
+        'training', help='Fine Tune a given diffusion model with LoRA'
+    )
+
+    inference_parser = subparsers.add_parser(
+        'inference', help='Inference of the fine-tuned diffusion model'
+    )
+
+    precomputed_parser = subparsers.add_parser(
+        'precompute', help='precomputed the data'
+    )
+    
+    merge_loras_v1_parser = subparsers.add_parser(
+        'merge_loras_v1', help='merge two loras v1'
+    )
+    
+    merge_loras_v2_parser = subparsers.add_parser(
+        'merge_loras_v2', help='merge two loras v2'
+    )
+    
+    merge_loras_v3_parser = subparsers.add_parser(
+        'merge_loras_v3', help='merge two loras v3'
+    )
+    
+    merge_loras_v4_parser = subparsers.add_parser(
+        'merge_loras_v4', help='merge two loras v4'
+    )
+
+
+    args = parser.parse_args()
+    main(args)
---
+++ src/scripts/merge_loras_v2.py
@@ -0,0 +1,55 @@
+from lora_diffusion import tune_lora_scale, monkeypatch_add_lora
+import torch
+from diffusers import (
+
+    StableDiffusionPipeline,
+    EulerAncestralDiscreteScheduler
+)
+
+# Linear inspired by cloneifsimo
+def merge_loras_v2(config: dict, base_dir: str, device: str):
+    
+    
+    state_dict_A=torch.load(config['w1'])
+    state_dict_B=torch.load(config['w2'])
+    
+    wd1 = {f"tensor_{i}": tensor for i, tensor in enumerate(state_dict_A)}
+    wd2 = {f"tensor_{i}": tensor for i, tensor in enumerate(state_dict_B)}
+    
+    unet_weights_A = {k: v for k, v in wd1.items() if "unet" in k}
+    text_encoder_weights_A = {k: v for k, v in wd1.items() if "text_encoder" in k}
+
+    unet_weights_B = {k: v for k, v in wd2.items() if "unet" in k}
+    text_encoder_weights_B = {k: v for k, v in wd2.items() if "text_encoder" in k}
+
+    pipe = StableDiffusionPipeline.from_pretrained(
+            config['general_model_name'],
+            torch_dtype=torch.float16,
+        ).to(device)
+        
+    pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    prompt='A womand cries in the snow, expressionism, renaissance.'
+    image = pipe(prompt, num_inference_steps=config['num_inference_steps'], guidance_scale=config['guidance_scale']).images[0]
+    image.save(f'{base_dir}/no_lora.png')
+    
+    monkeypatch_add_lora(pipe.unet, unet_weights_A)
+    monkeypatch_add_lora(pipe.text_encoder, text_encoder_weights_A, target_replace_module=["CLIPAttention"])
+   
+    torch.manual_seed(0)
+   
+    tune_lora_scale(pipe.unet, 0.7)
+    tune_lora_scale(pipe.text_encoder, 0.7)
+   
+
+    monkeypatch_add_lora(pipe.unet, unet_weights_B, alpha=1.0, beta = 1.0)
+    tune_lora_scale(pipe.unet, 0.4)
+    tune_lora_scale(pipe.text_encoder, 0.)
+
+    image = pipe(config['prompt'], num_inference_steps=config['num_inference_steps'], guidance_scale=config['guidance_scale']).images[0]
+    image.save(f'{base_dir}/lora.png')
+
+    
+
+
+
+    
