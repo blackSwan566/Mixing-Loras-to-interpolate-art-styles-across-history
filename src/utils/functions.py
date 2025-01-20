238b18d791c32adc81395b8fc7eb1213b2968f31
handler-bird <powerk11@hotmail.de>
2025-01-20 23:43:17 +0100

    save config and update path

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -27,6 +27,7 @@ def load_config(task: str) -> Tuple[dict, str]:
     elif 'merge' in task:
         dir_path = f'./src/data/{task}/run{config["version"]}/{config["style"]}'
 
+        # create folder if not exist
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
