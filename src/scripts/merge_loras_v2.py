16325b0284872e50987107a3c81f1c01ef69b0c8
handler-bird <powerk11@hotmail.de>
2025-01-22 10:01:09 +0100

    ruff formatting

--- src/scripts/merge_loras_v2.py
+++ src/scripts/merge_loras_v2.py
@@ -1,16 +1,19 @@
 import torch
-from lora_diffusion import monkeypatch_add_lora, tune_lora_scale, patch_pipe, monkeypatch_or_replace_lora
+from lora_diffusion import (
+    monkeypatch_add_lora,
+    tune_lora_scale,
+    patch_pipe,
+    monkeypatch_or_replace_lora,
+)
 from diffusers import (
     StableDiffusionPipeline,
 )
 
 
 def merge_loras_v2(config: dict, base_dir: str, device: str):
-    
     # Load weights from .pt
     lora1 = torch.load(config['w1'])
     lora2 = torch.load(config['w2'])
-
 
     pipe = StableDiffusionPipeline.from_pretrained(
         config['general_model_name'],
@@ -21,38 +24,36 @@ def merge_loras_v2(config: dict, base_dir: str, device: str):
     prompt = config['prompt']
 
     torch.manual_seed(config['seed'])
-    
+
     image = pipe(
         prompt,
         num_inference_steps=config['num_inference_steps'],
         guidance_scale=config['guidance_scale'],
     ).images[0]
     image.save(f'{base_dir}/no_lora_{prompt}.png')
-    
+
     monkeypatch_or_replace_lora(pipe.unet, lora1)
-    
+
     tune_lora_scale(pipe.unet, 0.7)
-    
+
     torch.manual_seed(config['seed'])
-    
+
     image = pipe(
         prompt,
         num_inference_steps=config['num_inference_steps'],
         guidance_scale=config['guidance_scale'],
-    ).images[0]    
+    ).images[0]
     image.save(f'{base_dir}/first_lora.png')
 
     monkeypatch_add_lora(pipe.unet, lora2, alpha=1, beta=1)
-    
+
     tune_lora_scale(pipe.unet, 0.7)
-    
+
     torch.manual_seed(config['seed'])
-    
+
     image = pipe(
         prompt,
         num_inference_steps=config['num_inference_steps'],
         guidance_scale=config['guidance_scale'],
-    ).images[0]    
+    ).images[0]
     image.save(f'{base_dir}/second_lora.png')
-
-
